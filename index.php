f7572782f968452bf85f4989513ac66a30ecc36c
Ronald Lamoreaux <gitkey@brownbros.net>
2018-04-26 01:53:13 -0400

    Allow multiple content souces
    The page can now load the main content from either a .php or .html file
    in the /pages directory, or from index.html or index.php in subdirectory
    of the /pages directory. In all cases file or subdirectory name is to be
    the page name used in the links created for navigation later.
    
    Page content locations for `contact` would be, in order of search:
    
    1. `/pages/contact.php`
    2. `/pages/contact.html`
    3. `/pages/contact/index.php`
    4. `/pages/contact/index.html`
    
    Includes an update to the `README` for typo fixes and status changes.

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<?php
-$page_name= filter_input(INPUT_SERVER, 'QUERY_STRING', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_BACKTICK);
-?>
 <html>
 <!--
- * Copyright ©2018 Ronald Lamoreaux, DBA Chindraba
+ * Copyright ©<?php echo date('Y');?> Ronald Lamoreaux, DBA Chindraba
  *
  * This work is not licensed for use or distribution.
 -->
+<?php
+include('cgi-bin/contact_handler.php');
+$page_name= filter_input(INPUT_SERVER, 'QUERY_STRING', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_BACKTICK);
+?>
 <?php include('cgi-bin/html_head.php'); ?>
 <body>
 <div id="page_box"><div id="page_wrapper">
@@ -14,9 +15,26 @@ $page_name= filter_input(INPUT_SERVER, 'QUERY_STRING', FILTER_SANITIZE_STRING, F
     include('cgi-bin/page_nav.php');
     include('cgi-bin/page_header.php');
 ?>
-<div id="content_wrapper"><?php include("pages/$page_name.php"); ?></div>
+<div id="content_wrapper"><?php
+  $page_source = getenv('DOCUMENT_ROOT') . '/pages/' . $page_name;
+  if ( file_exists("$page_source.php") ) {
+    include("$page_source.php");
+  } elseif ( file_exists("$page_source.html") ) {
+    readfile("$page_source.html");
+  } elseif ( file_exists("$page_source") && is_dir("$page_source") ){
+    if ( file_exists("$page_source/index.php") ) {
+      include("$page_source/index.php");
+    } elseif ( file_exists("$page_source/index.html") ) {
+      readfile("$page_source/index.html");
+    } else {
+      echo "<div><b>Cannot find a file to use for the '$page_name ' page.</b></div>\n";
+    }
+  } else {
+    echo "<div><b>Cannot find a directory or file to use for the '$page_name ' page.</b></div>\n";
+  }
+?></div>
 <?php include('cgi-bin/page_footer.php'); ?>
 </div>
-<div id="copyright">Site content Copyright &copy;2018 by Brown Bros. Heat &amp; Air.</div></div>
+<div id="copyright">Site content Copyright &copy;<?php echo date('Y');?> by Brown Bros. Heat &amp; Air.</div></div>
 </body>
 </html>
