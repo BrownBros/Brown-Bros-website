6ce630f10dc32fd47f13d55faa12e36fc9c5cc21
Ronald Lamoreaux <gitkey@brownbros.net>
2018-04-25 20:52:32 -0400

    Add the file for footer content

---
+++ cgi-bin/page_footer.php
@@ -0,0 +1,15 @@
+<footer>
+  <div>Any time you need us! Week-ends, holidays, we're here for you!</div>
+  <div>Visit: <a href="http://www.BrownBros.net">www.BrownBros.net</a></div>
+  <div>Contact: <a href="mailto:mike&#64;BrownBros&#46;net">mike&#64;BrownBros&#46;net</a></div>
+  <div>All major credit cards accepted</div>
+  <div>
+    <img class="card_logo" src="cards/visa.png" />
+    <img class="card_logo" src="cards/master.png" />
+    <img class="card_logo" src="cards/amex.png" />
+    <img class="card_logo" src="cards/discover.png" />
+<!-- Diners Club card acceptance is not yet confirmed
+    <img class="card_logo" src="cards/diners.png" />
+-->
+  </div>
+</footer>
