f894bb2cf8e9ec38ab83000486cadfb9e0f65d1b
Ronald Lamoreaux <gitkey@brownbros.net>
2018-04-26 04:59:47 -0400

    Move remaining configuables to the data file
    Until further development needs configuration the extraction of data
    that can be placed in a configuration file should be finalized.

--- cgi-bin/page_nav.php
+++ cgi-bin/page_nav.php
@@ -5,13 +5,11 @@
     <span></span>
     <span></span>
     <ul id="menu">
-      <li><a href='/home' title='Home'>Home</a></li>
-      <li><a hrep='/specials' title="Specials">Specials</a></li>
-      <li><a href='/about' title='About Us'>About Us</a></li>
-      <li><a href='/info' title='Information'>Information</a></li>
-      <li><a href='/contact' title='Contact Us'>Contact Us</a></li>
-      <li><a href='/faq' title='FAQ'>FAQ</a></li>
+<?php
+        foreach ($page_links as $page_link) {
+          echo $page_link;
+        }?>
     </ul>
   </div>
-  <div class="crumb">Brown Bros. Heat &amp; Air</div>
+  <div class="crumb"><?php echo $page_crumb; ?></div>
 </nav>
