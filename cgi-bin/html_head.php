cd8dc30139cffd40b55abe1358f5f5d6ceceb401
Ronald Lamoreaux <gitkey@brownbros.net>
2018-04-25 22:13:12 -0400

    Factor out the common HTML HEAD content

---
+++ cgi-bin/html_head.php
@@ -0,0 +1,10 @@
+<head>
+  <title>Brown Bros. Air Conditioning &amp; Heating Sales &amp; Service</title>
+  <meta name="author" content="Ronald Lamoreaux, DBA Chindraba" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="description" content="Brown Bros Air Conditioning and Heating Sales and Service" />
+  <meta name="keywords" content="air conditioning, heating, sales, service, repair, installation" />
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="css/site-style.css" media="screen, handheld" />
+  <link rel="stylesheet" type="text/css" href="css/content-style.css" media="screen, handheld" />
+</head>
