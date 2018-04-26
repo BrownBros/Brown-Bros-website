6a36ba4689b5d88b2b4ce0a6e6ba56814bfd1d6a
Ronald Lamoreaux <gitkey@brownbros.net>
2018-04-25 20:58:04 -0400

    Factor out the common page footer content

--- no_thanks.php
+++ no_thanks.php
@@ -45,22 +45,7 @@
   </form>
 
 </div>
-<footer>
-  <div>Any time you need us! Week-ends, holidays, we're here for you!</div>
-  <div>Visit: <a href="http://www.BrownBros.net">www.BrownBros.net</a></div>
-  <div>Contact: <a href="mailto:mike&#64;BrownBros&#46;net">mike&#64;BrownBros&#46;net</a></div>
-  <div>All major credit cards accepted</div>
-  <div>
-    <img class="card_logo" src="cards/visa.png" />
-    <img class="card_logo" src="cards/master.png" />
-    <img class="card_logo" src="cards/amex.png" />
-    <img class="card_logo" src="cards/discover.png" />
-<!-- Diners Club card acceptance is not yet confirmed
-    <img class="card_logo" src="cards/diners.png" />
--->
-  </div>
-</footer>
-
+<?php include('cgi-bin/page_footer.php'); ?>
 </div>
 <div id="copyright">Site content Copyright &copy;2018 by Brown Bros. Heat &amp; Air.</div></div>
 </body>
